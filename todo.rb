982b42c3a0dcae3b83283b94c88d09d9ce6f57c8
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2016-12-29 17:11:47 +0000

    add Task class; create add method to add a task to the all tasks array

--- todo.rb
+++ todo.rb
@@ -1,6 +1,15 @@
+# Create a list that manages the behavior of an individual list
 class List
-  # Create a list
-  # Add task to list
+  attr_reader :all_tasks
+
+  def initialize
+    @all_tasks = []
+  end
+
+  # Add tasks to list
+  def add(task)
+    @all_tasks << task
+  end
   # Show all tasks
   # Read a task from a file
   # Write a list to a file
@@ -8,12 +17,21 @@ class List
   # Update a task
 end
 
-# class Task
+# manages the behavior of each individual task
+class Task
+  attr_reader :description
+
+  #description upon creation of each instance
+  def initialize(description)
+    @description = description
+  end
 #   Create a task item
-# end
+end
 
 #program runner
 if __FILE__ == $PROGRAM_NAME
   my_list = List.new
   puts "you have created a new list"
+  task_1 = my_list.add(Task.new("tidy up"))
+  puts "You have added a task to the Todo List"
 end
