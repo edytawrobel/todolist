cd89320c5f3781ae4011a84a58e4ef5a704915c0
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2016-12-29 16:34:32 +0000

    add program runner

---
+++ todo.rb
@@ -0,0 +1,19 @@
+class List
+  # Create a list
+  # Add task to list
+  # Show all tasks
+  # Read a task from a file
+  # Write a list to a file
+  # Delete a task
+  # Update a task
+end
+
+# class Task
+#   Create a task item
+# end
+
+#program runner
+if __FILE__ == $PROGRAM_NAME
+  my_list = List.new
+  puts "you have created a new list"
+end
